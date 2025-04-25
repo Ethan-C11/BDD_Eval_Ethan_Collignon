1b4c133edc43e8c048b0a541e5d524411440043e
Ethan-C11 <ethan.collignon.1102@gmail.com>
2025-04-25 14:00:33 +0200

    CommonStep update

--- BDD_Eval_Ethan_Collignon/StepDefinitions/CommonStepDefinitions.cs
+++ BDD_Eval_Ethan_Collignon/StepDefinitions/CommonStepDefinitions.cs
@@ -9,9 +9,15 @@ namespace BDD_Eval_Ethan_Collignon.StepDefinitions
     public class CommonStepDefinitions
     {
         [Given("La page actuelle est {string}")]
-        public void GivenLaPageActuelleEst(string p0)
+        public void GivenLaPageActuelleEst(string title)
         {
-            Assert.AreEqual(PageHelper.GetPageTitle(), p0);
+            Assert.AreEqual(PageHelper.GetPageTitle(), title);
+        }
+
+        [Given("L'element dont le XPath est {string} est présent")]
+        public void GivenLelementDontLeXPathEstEstPresent(string xPath)
+        {
+            Assert.IsTrue(GenericHelper.IsElementPresentOnce(By.XPath(xPath)));
         }
 
         [When("Je remplis l'input {string} avec la valeur {string}")]
@@ -26,10 +32,34 @@ namespace BDD_Eval_Ethan_Collignon.StepDefinitions
             ButtonHelper.ClickButton(By.Name(nom));
         }
 
-        [Then("La page actuelle est {string}")]
-        public void ThenLaPageActuelleEst(string p0)
+        [When("je clique sur le bouton dont le XPath est {string}")]
+        public void WhenJeCliqueSurLeBoutonDontLeXPathEst(string xPath)
         {
-            Assert.AreEqual(PageHelper.GetPageTitle(), p0);
+            ButtonHelper.ClickButton(By.XPath(xPath));
+        }
+
+        [When("je confirme le dialogue")]
+        public void WhenJeConfirmeLeDialogue()
+        {
+            AlertHelper.Accept();
+        }
+
+        [When("je refuse le dialogue")]
+        public void WhenJeRefuseLeDialogue()
+        {
+            AlertHelper.Dismiss();
+        }
+
+        [Then("La page actuelle est {string}")]
+        public void ThenLaPageActuelleEst(string title)
+        {
+            Assert.AreEqual(PageHelper.GetPageTitle(), title);
+        }
+
+        [Then("L'element dont le XPath est {string} existe")]
+        public void ThenLelementDontLeXPathEstExiste(string xPath)
+        {
+            Assert.IsTrue(GenericHelper.IsElementPresentOnce(By.XPath(xPath)));
         }
     }
 }
